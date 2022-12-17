7bd4a756b86bba2c03f96525eaddea5c59f2104a
vwu0000 <vwu00000@email.cpcc.edu>
2022-12-17 13:25:03 -0500

    pushing to work on different computer

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,7 +8,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-
+import frc.robot.utils.AutoBuilder;
 
 
 /**
@@ -22,7 +22,11 @@ public class Robot extends TimedRobot
     private Command autonomousCommand;
     
     private RobotContainer robotContainer;
-    
+
+    public enum AutoModes {
+    }
+    private AutoBuilder autoBuilder;
+
     
     /**
      * This method is run when the robot is first started up and should be used for any
---
+++ src/main/java/frc/robot/utils/AutoBuilder.java
@@ -0,0 +1,31 @@
+package frc.robot.utils;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Robot.AutoModes;
+import frc.robot.RobotContainer;
+
+public class AutoBuilder {
+    private RobotContainer robotContainer;
+    private AutoModes autoMode;
+    private SequentialCommandGroup autoCommand;
+    private AutoPath startPath;
+
+    public Command build() {
+        autoCommand = new SequentialCommandGroup();
+
+        switch (autoMode) {
+        }
+
+        autoCommand.beforeStarting(new InstantCommand(() -> robotContainer.drivetrainSubsystem.resetOdometer(startPath.getInitPose())));
+        return autoCommand;
+    }
+
+    public void setRobotContainer(RobotContainer robotContainer) {
+        this.robotContainer = robotContainer;
+    }
+    public void setAutoMode(AutoModes autoMode) {
+        this.autoMode = autoMode;
+    }
+}
